ea04cc331e3a52c48fe4d0fb2ca98df770052e0d
Artem Zavialov <z.artem96@gmail.com>
2021-05-13 23:18:11 +0500

    задание 2, попытка 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,11 +6,11 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingPrice
-    let shippingPrice;
+    let shippingSum;
     if (productsSum === 0 || productsSum >= freeShippingMinSum) {
-        shippingPrice = 0;
+        shippingSum = 0;
     } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
-        shippingPrice = shipping;
+        shippingSum = shipping;
     }
     // если productsSum равно 0,
     // то shippingPrice присвоить значение 0
